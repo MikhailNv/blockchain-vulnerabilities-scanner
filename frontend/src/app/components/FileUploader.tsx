82c204a392e4a297e2b1e29f116009ae498dab8d
Ungadult <mihail20022015@gmail.com>
2023-11-05 21:25:55 +0300

    add upload files and generate dates

--- frontend/src/app/components/FileUploader.tsx
+++ frontend/src/app/components/FileUploader.tsx
@@ -1,22 +1,16 @@
 import { BlobReader, ZipReader } from "@zip.js/zip.js";
-import fileToArrayBuffer from 'file2arraybuffer'
 import {FC} from 'react'
 import { useState } from 'react';
-import { serverAction } from "./ServerAction";
+import { serverAction, getYearRangeNvdCve, sss } from "./ServerAction";
 
-async function execCommand(package_name: string) {
-    // 'use server';
-    // const req = await fetch("/api/cves?" + new URLSearchParams({package_name: package_name}), {
-    //     method: 'GET',
-    //     headers: {
-    //       accept: 'application/json',
-    //     },
-    // });
-    // const data = await req.json();
-    // const cpe: string = await serverAction(package_name);
+import { InboxOutlined } from '@ant-design/icons';
+import type { UploadProps } from 'antd';
+import { message, Upload } from 'antd';
 
-    console.log("cpe");
-}
+import { Select, Space } from 'antd';
+import type { SelectProps } from 'antd';
+
+const { Dragger } = Upload;
 
 async function getFileNamesFromZip(e) {
     const file = e.target.files[0];
@@ -29,10 +23,60 @@ async function getFileNamesFromZip(e) {
 }
 
 const FileUploader: FC = () => {
-    const [selectedFile, setSelectedFile] = useState<File>();
+    const [options, setOptions] = useState<SelectProps['options']>([]);
+    const [fileState, setFileState] = useState<boolean>(false);
+    const [years, setYears] = useState<boolean>(false);
+
+    const handleChange = (value: string[]) => {
+        if (value.length > 0)
+        {
+            setYears(true);
+        }
+        else
+        {
+            setYears(false);
+        }
+    };
+
+    const props: UploadProps = {
+        name: 'file',
+        multiple: true,
+        action: 'https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188',
+        beforeUpload: async (file) => {
+            const options: SelectProps['options'] = [];
+            const res = await getYearRangeNvdCve();
+            for (var year of res) {
+                options.push({
+                    label: year,
+                    value: year,
+                });
+            }
+            setOptions(options);
+            setFileState(true);
+            return false;
+        },
+        onChange(info) {
+          const { status } = info.file;
+          console.log(status);
+          if (status !== 'uploading') {
+            console.log(info.file, info.fileList);
+          }
+          if (status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully.`);
+          } else if (status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+          }
+        },
+        onDrop(e) {
+          console.log('Dropped files', e.dataTransfer.files);
+        },
+        onRemove(e) {
+            setOptions([]);
+            setFileState(false);
+        }
+    };
     return (
     <div className="relative flex justify-center py-12 px-4 sm:px-6 lg:px-8 bg-no-repeat bg-cover relative">
-        {/* <div className="absolute bg-black opacity-60 inset-0 z-0"></div> */}
         <div className=" w-full p-10 bg-white rounded-xl z-10">
             <div className="text-center">
                 <h2 className="mt-5 text-3xl font-bold text-gray-900">
@@ -40,38 +84,69 @@ const FileUploader: FC = () => {
                 </h2>
                 <p className="mt-2 text-sm text-gray-400">Введите название дистрибутива</p>
             </div>
-            <form className="mt-8 space-y-3" action="#" method="POST">
-                        <div className="grid grid-cols-1 space-y-2">
-                            <label className="text-sm font-bold text-gray-500 tracking-wide">Title</label>
-                                <input className="text-base p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500" type="" placeholder="Alt Linux" />
-                        </div>
-                        <div className="grid grid-cols-1 space-y-2">
-                                        <label className="text-sm font-bold text-gray-500 tracking-wide">Attach Document</label>
-                            <div className="flex items-center justify-center w-full">
-                                <label className="flex flex-col rounded-lg border-4 border-dashed w-full h-60 p-10 group text-center">
-                                    <div className="h-full w-full text-center flex flex-col items-center justify-center items-center  ">
-                                        {/* <!---<svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 text-blue-400 group-hover:text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
-                                        </svg>--> */}
-                                        <div className="flex flex-auto max-h-48 w-2/5 mx-auto -mt-10">
-                                        <img className="has-mask h-36 object-center" src="https://img.freepik.com/free-vector/image-upload-concept-landing-page_52683-27130.jpg?size=338&ext=jpg" alt="freepik image" />
-                                        </div>
-                                        <p className="pointer-none text-gray-500 "><span className="text-sm">Перенесите</span> файл сюда <br /> или выберите файл на вашем компьютере</p>
-                                    </div>
-                                    <input type="file" className="hidden" onChange={getFileNamesFromZip}/>
-                                </label>
+            {/* <form className="mt-8 space-y-3" action="#" method="POST">
+                <div className="grid grid-cols-1 space-y-2">
+                    <label className="text-sm font-bold text-gray-500 tracking-wide">Title</label>
+                        <input className="text-base p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500" type="" placeholder="Alt Linux" />
+                </div>
+                <div className="grid grid-cols-1 space-y-2">
+                                <label className="text-sm font-bold text-gray-500 tracking-wide">Attach Document</label>
+                    <div className="flex items-center justify-center w-full">
+                        <label className="flex flex-col rounded-lg border-4 border-dashed w-full h-60 p-10 group text-center">
+                            <div className="h-full w-full text-center flex flex-col items-center justify-center items-center  ">
+                                <div className="flex flex-auto max-h-48 w-2/5 mx-auto -mt-10">
+                                <img className="has-mask h-36 object-center" src="https://img.freepik.com/free-vector/image-upload-concept-landing-page_52683-27130.jpg?size=338&ext=jpg" alt="freepik image" />
+                                </div>
+                                <p className="pointer-none text-gray-500 "><span className="text-sm">Перенесите</span> файл сюда <br /> или выберите файл на вашем компьютере</p>
                             </div>
-                        </div>
-                                <p className="text-sm text-gray-300">
-                                    <span>Тип файлов: doc,pdf,types of images</span>
-                                </p>
-                        <div>
-                            <button type="submit" className="my-5 w-full flex justify-center bg-blue-500 text-gray-100 p-4  rounded-full tracking-wide
-                                        font-semibold  focus:outline-none focus:shadow-outline hover:bg-blue-600 shadow-lg cursor-pointer transition ease-in duration-300">
-                            Загрузка
-                        </button>
-                        </div>
-            </form>
+                            <input type="file" className="hidden" onChange={getFileNamesFromZip}/>
+                        </label>
+                    </div>
+                </div>
+                        <p className="text-sm text-gray-300">
+                            <span>Тип файлов: doc,pdf,types of images</span>
+                        </p>
+                <div>
+                    <button type="submit" className="my-5 w-full flex justify-center bg-blue-500 text-gray-100 p-4  rounded-full tracking-wide
+                                font-semibold  focus:outline-none focus:shadow-outline hover:bg-blue-600 shadow-lg cursor-pointer transition ease-in duration-300">
+                    Загрузка
+                    </button>
+                </div>
+            </form> */}
+            <div className="mt-8 space-y-3">
+                <Dragger {...props}>
+                    <p className="ant-upload-drag-icon">
+                        <InboxOutlined />
+                    </p>
+                    <p className="ant-upload-text">Нажмите или перетащите файл в эту область, чтобы загрузить</p>
+                    <p className="ant-upload-hint">
+                    Поддержка одиночной или массовой загрузки. Строго запрещено загружать данные компании или другие
+                    запрещенные файлы.
+                    </p>
+                </Dragger>
+            </div>
+            {fileState ? 
+                <div className="mt-8 space-y-3">
+                    <Space style={{ width: '100%' }} direction="vertical">
+                    <Select
+                    mode="multiple"
+                    allowClear
+                    style={{ width: '100%' }}
+                    placeholder="Please select"
+                    onChange={handleChange}
+                    options={options}
+                    />
+                    </Space>
+                </div>
+                : <></>
+            }
+            <div className="mt-8 space-y-3">
+                <button type="button"
+                className="my-5 w-full flex justify-center bg-blue-500 text-gray-100 p-4 rounded-full tracking-wide font-semibold focus:outline-none focus:shadow-outline hover:bg-blue-600 shadow-lg transition ease-in duration-300 disabled:opacity-50"
+                disabled={years ? false : true}>
+                Загрузка
+                </button>
+            </div>
         </div>
     </div>
 
--- frontend/src/app/components/ServerAction.ts
+++ frontend/src/app/components/ServerAction.ts
@@ -10,10 +10,11 @@ const __feeds : string = __approot + "/feeds";
 export async function serverAction(req: Array<string>) {
     const cpes_array: Array<string> = [];
 
-    const output_nvdsync = execSync(`nvdsync -v=1 -cve_feed=cve-1.1.json.gz ${__feeds}`, { shell: '/bin/bash', encoding: 'utf-8' });
-    const output_rpm2cpe = execSync(`rpm2cpe -rpm=1 -cpe=2 -e=1 << EOF ${req.join("\n")}\nEOF`, { shell: '/bin/bash', encoding: 'utf-8' });
+    // const output_nvdsync = execSync(`nvdsync -v=1 -cve_feed=cve-1.1.json.gz ${__feeds}`, { shell: '/bin/bash', encoding: 'utf-8' });
+    const output_rpm2cpe = execSync(`rpm2cpe -rpm 1 -cpe 2 -e 1 << EOF ${req.join("\n")}\nEOF`, { shell: '/bin/bash', encoding: 'utf-8' });
     const splitted_output_rpm2cpe = output_rpm2cpe.replace(/\:alt.*\n/gi, '\n');
-    const output_cpe2cve = execSync(`cpe2cve -cpe 1 -e 1 -cve 1 ${__feeds}/nvdcve-1.1-*.json.gz << EOF ${splitted_output_rpm2cpe}\nEOF`, { shell: '/bin/bash', encoding: 'utf-8' });
+    // console.log(execSync(`cpe2cve -cpe 1 -e 1 -cve 1 ${__feeds}/nvdcve-1.1-*.json.gz << EOF\n${splitted_output_rpm2cpe.split('\n')[0]}\nEOF`, { shell: '/bin/bash', encoding: 'utf-8' }));
+    const output_cpe2cve = execSync(`cpe2cve -cpe 1 -e 1 -cve 1 ${__feeds}/nvdcve-1.1-*.json.gz << EOF\n${splitted_output_rpm2cpe.split('\n')[0]}\nEOF`, { shell: '/bin/bash', encoding: 'utf-8' });
     const splitted = output_cpe2cve.split(/\r?\n/);
     const filtered = splitted.filter( (e: string) => {
       return e !== '';
@@ -22,3 +23,23 @@ export async function serverAction(req: Array<string>) {
 
     return(cpes_array);
 }
+
+export async function getYearRangeNvdCve() {
+  const years_array: Array<string> = [];
+  const output_rpm2cpe = execSync(`ls ${__feeds}/*.json.gz`, { shell: '/bin/bash', encoding: 'utf-8' });
+  for (var cve_json of output_rpm2cpe.split(/\r?\n/)) {
+    const year: string = cve_json.split("-").at(-1).split(".")[0];
+    if (year.length == 4)
+    {
+      years_array.push(year);
+    }
+  }
+  return(years_array);
+}
+
+export const sss = async() => {
+  return("yes");
+}
+// export async function sss() {
+//   return("yes");
+// }
